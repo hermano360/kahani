37a22ccd7cb2f51c2e50e946bebb127aad4d55f5
Herminio Garcia <hermano360@gmail.com>
2023-01-07 21:47:53 -0800

    Handle rectangle drawings

--- src/hooks/useCanvas.tsx
+++ src/hooks/useCanvas.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef } from "react";
+import { RectInstance, CanvasHandlers } from "../types";
 
 const drawBorder = (ctx: CanvasRenderingContext2D) => {
   ctx.clearRect(0, 0, 500, 500);
@@ -9,27 +10,71 @@ const drawBorder = (ctx: CanvasRenderingContext2D) => {
   ctx.stroke();
 };
 
-export const useCanvas = () => {
-  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+const drawRectangles = (
+  ctx: CanvasRenderingContext2D,
+  rectangles: RectInstance[]
+) => {
+  rectangles.forEach((rect) => {
+    ctx.beginPath();
+    ctx.lineWidth = 1;
+    ctx.rect(rect.x1, rect.y1, rect.x2 - rect.x1, rect.y2 - rect.y1);
+    ctx.fillStyle = `rgb(${rect.color[0]},${rect.color[1]},${rect.color[2]})`;
+    ctx.fill();
+    ctx.strokeStyle = "#000000";
+    ctx.stroke();
+  });
+};
 
-  const handleDownClick = () => {};
-  const handleDoubleClick = () => {};
-  const handleUpClick = () => {};
-  const handleMouseMove = () => {};
+export const useCanvas = (
+  rectangles: RectInstance[],
+  {
+    handleDoubleClick,
+    handleDownClick,
+    handleUpClick,
+    handleMouseMove,
+  }: CanvasHandlers
+) => {
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
   useEffect(() => {
     const canvas = canvasRef.current;
     if (!canvas) return;
+
     const context = canvas.getContext("2d");
     if (!context) return;
 
-    canvas.onmousedown = handleDownClick;
-    canvas.onmouseup = handleUpClick;
-    canvas.ondblclick = handleDoubleClick;
-    canvas.onmousemove = handleMouseMove;
+    canvas.onmousedown = (e) => {
+      const x = e.offsetX;
+      const y = e.offsetY;
+
+      handleDownClick(x, y);
+    };
+    canvas.onmouseup = (e) => {
+      const x = e.offsetX;
+      const y = e.offsetY;
+
+      handleUpClick(x, y);
+    };
+    canvas.ondblclick = (e) => {
+      const x = e.offsetX;
+      const y = e.offsetY;
+      handleDoubleClick(x, y);
+    };
+    canvas.onmousemove = (e) => {
+      const x = e.offsetX;
+      const y = e.offsetY;
+      handleMouseMove(x, y);
+    };
 
     drawBorder(context);
-  }, []);
+    drawRectangles(context, rectangles);
+  }, [
+    handleDoubleClick,
+    handleDownClick,
+    handleMouseMove,
+    handleUpClick,
+    rectangles,
+  ]);
 
   return { canvasRef };
 };
